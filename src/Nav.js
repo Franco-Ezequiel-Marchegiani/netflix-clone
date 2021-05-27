c1e2069d91b10058eb6a151fbce694115d48d6da
Franco Ezequiel <eze.cavs@hotmail.com>
2021-05-27 00:37:36 -0300

    Implementé el navbar, está terminado y realicé modificaciones, solucioné el scroll horizontal, era un problema de padding horizontal en las columnas, y agrandé un poco más la imagen del banner y la dejé a 520px

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./Nav.css";
 function Nav() {
+    const [show, handleShow] = useState(false)
+    useEffect(() =>{
+        window.addEventListener("scroll", () =>{
+            if (window.scrollY > 100){
+                handleShow(true)
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeAddEventListener("scroll");
+        };
+    }, []);
+
     return (
-        <div>
-            
+        <div className={`nav ${show && "nav__black"}`}>
+            <img className="nav__logo" src="https://logos-marcas.com/wp-content/uploads/2020/04/Netflix-Logo.png" alt="Netflix Logo"></img>
+            <img className="nav__avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix Logo"></img>
         </div>
     )
 }
