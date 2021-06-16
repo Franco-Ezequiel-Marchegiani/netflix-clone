031ec783f7644df14440f7c529301928e7201ad1
Franco Ezequiel <eze.cavs@hotmail.com>
2021-06-15 22:42:09 -0300

    Modificación imagen logo Netflix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,7 +15,7 @@ function Nav() {
 
     return (
         <div className={`nav ${show && "nav__black"}`}>
-            <img className="nav__logo" src="https://logos-marcas.com/wp-content/uploads/2020/04/Netflix-Logo.png" alt="Netflix Logo"></img>
+            <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"></img>
             <img className="nav__avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix Logo"></img>
         </div>
     )
